6f2aea84526b50887327b63e45a9088c15abe804
Dmitry Stepanenko <sdm102@yandex.ua>
2019-03-18 17:04:29 +0200

    Add ability to parse arrays of values.

--- lib/localio/segment.rb
+++ lib/localio/segment.rb
@@ -2,9 +2,9 @@ class Segment
 
   attr_accessor :key, :translation, :language, :nested
 
-  def initialize(key, translation, language)
+  def initialize(key, translation, language, options={})
     @key = key
-    @translation = translation.to_s.replace_escaped
+    @translation = options[:raw_translation] && translation || translation.to_s.replace_escaped
     @language = language
     @nested = []
   end
